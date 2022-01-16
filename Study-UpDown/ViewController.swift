983c4894a3f3227b9f3a321adf54eb390e78d1be
MANSU.J <seunghyun.jung.84@gmail.com>
2022-01-16 19:55:38 +0900

    File Import

--- Study-UpDown/ViewController.swift
+++ Study-UpDown/ViewController.swift
@@ -1,18 +1,78 @@
 //
 //  ViewController.swift
-//  Study-UpDown
+//  UpDown
 //
-//  Created by SH.Jung on 2022/01/16.
+//  Created by SH.Jung on 2021/03/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK:- Properies
+    // MARK: IBOutlets
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var turnCountLabel: UILabel!
+    @IBOutlet var inputField: UITextField!
+    
+    // MARK: Stored Properties
+    var randomNumber: UInt32 = 0
+    var turnCount: Int = 0
+        
+    // MARK:- Methods
+    @IBAction func touchUpSubmitButton(_ sender: UIButton) {
+        
+        guard let inputText = self.inputField.text, inputText.isEmpty == false else {
+            print("입력값 없음")
+            return
+        }
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
+        guard let inputNumber: UInt32 = UInt32(inputText) else {
+            print("입력값이 잘못 되었음")
+            return
+        }
+        
+        turnCount += 1
+        self.turnCountLabel.text = "\(turnCount)"
+        
+        if inputNumber > randomNumber {
+            self.resultLabel.text = "UP!!"
+        } else if inputNumber < randomNumber {
+            self.resultLabel.text = "DOWN!!"
+        } else {
+            self.resultLabel.text = "정답입니다!"
+        }
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("touch up reset button")
+    }
+    @IBAction func tapBackground(_ sender: UITapGestureRecognizer) {
+//        self.view.endEditing(true)
+//        self.inputField.resignFirstResponder()
+        self.inputField.endEditing(true)
+
+        print("tap background view")
+    }
+    
+    // MARK: Custom Methods
+    func initializeGame() {
+        self .randomNumber = arc4random() % 25
+        self.turnCount = 0
+        
+        self.resultLabel.text = "Start!"
+        self.turnCountLabel.text = "\(turnCount)"
+        self.inputField.text = nil
+        
+        print("임의의 숫자 \(self.randomNumber)")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.initializeGame()
+    }
 
 
 }
